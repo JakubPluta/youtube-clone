ba93f93d11f025a4ad9cc827a2fbd93908941755
jakubpluta <plutakuba@gmail.com>
2022-01-02 14:58:14 +0100

    Created react app for youtube clone

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+import { Grid } from '@material-ui/core'
+import youtube from "./api/youtube";
+import {VideoDetail, VideoList, SearchBar} from './components';
+
+class App extends React.Component {
+    handleSubmit = async (searchTerm) => {
+        const response = await youtube.get('search', {
+        params: {
+            part: 'snippet',
+            maxResults:5,
+            key: process.env.REACT_APP_API_KEY,
+            q: searchTerm,
+    
+        }
+    }
+        
+        
+        )
+        
+        
+        ;
+        console.log(response.data)
+    }
+render () {
+    return (
+    <Grid justifyContent='center' container spacing={10}> 
+        <Grid item xs={12}>
+            <Grid container spacing={10}> 
+                <Grid item xs={12}>
+                <SearchBar onFormSubmit={this.handleSubmit}/>
+                </Grid>
+
+                <Grid item xs={8}>
+                <VideoDetail/>
+                </Grid>
+
+                <Grid item xs={4}>
+                    {/* {Video List} */}
+                </Grid>
+            </Grid>
+        </Grid>
+    </Grid>
+    )
 }
+
+}
+
+
+
+
+// const App = () => {
+//     return <h1>Youtube clone</h1>
+// }
 
 export default App;
